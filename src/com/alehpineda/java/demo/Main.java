6bc2dcc9fa44c4335e7d100efe21dec733a802de
Alex Pineda <h0h04dt@homeoffice.wal-mart.com>
2021-07-30 03:03:27 -0500

    Singleton with static block

--- src/com/alehpineda/java/demo/Main.java
+++ src/com/alehpineda/java/demo/Main.java
@@ -92,5 +92,35 @@ public class Main {
         System.out.println("String from y3 is "+y3.s);
         System.out.println("String from z3 is "+z3.s);
         System.out.println("\n");
+
+        // Singleton with static block
+        SingletonWithStaticBlock x4 = SingletonWithStaticBlock.instance;
+        SingletonWithStaticBlock y4 = SingletonWithStaticBlock.instance;
+        SingletonWithStaticBlock z4 = SingletonWithStaticBlock.instance;
+
+        System.out.println("String from x4 is "+x4.s);
+        System.out.println("String from y4 is "+y4.s);
+        System.out.println("String from z4 is "+z4.s);
+        System.out.println("\n");
+
+        // Upper case
+        // Changing variable of instance of x1
+        x4.s = (x4.s).toUpperCase();
+
+        // All will be uppercase
+        System.out.println("String from x4 is "+x4.s);
+        System.out.println("String from y4 is "+y4.s);
+        System.out.println("String from z4 is "+z4.s);
+        System.out.println("\n");
+
+        // Lower case
+        // Changing variable of instance of x1
+        x4.s = (x4.s).toLowerCase();
+
+        // All will be uppercase
+        System.out.println("String from x4 is "+x4.s);
+        System.out.println("String from y4 is "+y4.s);
+        System.out.println("String from z4 is "+z4.s);
+        System.out.println("\n");
     }
 }
---
+++ src/com/alehpineda/java/demo/SingletonWithStaticBlock.java
@@ -0,0 +1,20 @@
+package com.alehpineda.java.demo;
+
+// Java code to create singleton class
+// Using static block
+public class SingletonWithStaticBlock {
+    // public instance
+    public static SingletonWithStaticBlock instance;
+
+    // Public String variable
+    String s;
+    // private constructor
+    private SingletonWithStaticBlock(){
+        s = "Hello world from a Singleton with Static Block";
+    }
+
+    // static block to initialize instance
+    static {
+        instance = new SingletonWithStaticBlock();
+    }
+}
